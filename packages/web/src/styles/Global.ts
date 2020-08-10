1b552502a4ea10c0a1a1289269a4480e90660532
Derli Machado <dam.machado1@gmail.com>
2020-08-10 17:41:52 -0300

    Globalstyles and font.

--- packages/web/src/styles/Global.ts
+++ packages/web/src/styles/Global.ts
@@ -2,25 +2,21 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
     *{
-        margin: 0;
-        padding: 0;
-        outline: 0;
+        margin:0;
+        padding:0;
+        outline:0;
         box-sizing: border-box;
-
     }
-
-    body {
-        background: #c6cfcf ;
-        -webkit-font-smoothing: antialiased
-    }
-
+    
     body, input, button {
-        font-family: 'Roboto Slab', serif;
-        font-size: 16px;
+        font: 16px, Roboto, sans-serif;
     }
-
+    #root{
+        max-width: 960px;
+        margin: 0 auto;
+        padding: 40px 20px;
+    }
     button {
-        cursor:pointer;
-
+        cursor: pointer;
     }
 `
