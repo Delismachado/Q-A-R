264c6644db6d4d79b18bd80cc97ad1b393a4bcf3
Lucas Marcondes Pavelski <lmpavelski@yahoo.com.br>
2020-08-29 08:41:51 -0300

    Add: new production api domain.

--- packages/web/src/services/api.ts
+++ packages/web/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'https://qar.derlimachado.xyz'
+      ? 'https://qar-api.derlimachado.xyz'
       : 'http://localhost:3333'
 })
 
